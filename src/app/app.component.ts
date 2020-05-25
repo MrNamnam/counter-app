49287f974a45dd5fcfa5b8be52462139c350d401
Bar Naim <bar.naim@similarweb.com>
2020-05-25 22:00:40 +0300

    real projrect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,32 @@
-import { Component } from "@angular/core";
+import { Component, ViewEncapsulation, ViewChild} from "@angular/core";
 import * as signalR from "@aspnet/signalr";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { map, switchMap } from "rxjs/operators";
+import {MatSelectionList, MatListOption} from '@angular/material/list';
+import {FlatTreeControl} from '@angular/cdk/tree';
+import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
+import {MatIconModule} from '@angular/material/icon';
+import { range } from 'rxjs';
+import { stringify } from 'querystring';
+
+
+interface SomkingDetector {
+  id: number
+  latitude: number
+  longitude: number
+  address: string
+  owner_name: string
+  owner_phone_num: number 
+}
+
+interface Events {
+  Timestamp: Date
+  device_id: number
+  event_id: number
+  is_false_alarm: boolean
+  event_details: string
+  num_of_injured: number
+}
 
 interface SignalRConnection {
   url: string;
@@ -13,53 +38,171 @@ interface Counter {
   count: number;
 }
 
+interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
+
+interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'Mike',
+    children: [
+      {name: 'home device'},
+      {name: 'shop device'},
+      {name: 'garage device'},
+    ]
+  }, {
+    name: 'Charly',
+    children: [
+      {name: 'home device'},
+      {name: 'garage device'},
+    ]
+  }, {
+        name: 'Anne',
+        children: [
+          {name: 'home device'},
+        ]
+  },
+];
+
+/** Flat node with expandable and level information */
+interface ExampleFlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+}
+
+
+interface AlertNode {
+  alert_obj: Object;
+  client_obj: Object
+}
+
+
+
+
+
+
+
+
+
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.less"]
+  styleUrls: ["./app.component.css"],
+  encapsulation: ViewEncapsulation.None,
 })
+
+
+
 export class AppComponent {
+  panelOpenState = false;
+  checked = false;
+  injured = ""
+
+  
+
   private readonly httpOptions = { headers: new HttpHeaders({ "Content-Type": "application/json" }) };
   private readonly negotiateUrl = "https://cors-anywhere.herokuapp.com/https://counterfunctions20200425175523.azurewebsites.net/api/negotiate";
   private readonly getCounterUrl = "https://cors-anywhere.herokuapp.com/https://counterfunctions20200425175523.azurewebsites.net/api/get-counter";
   private readonly updateCounterUrl = "https://cors-anywhere.herokuapp.com/https://counterfunctions20200425175523.azurewebsites.net/api/update-counter";
   private readonly getIotDevicesUrl = "https://cors-anywhere.herokuapp.com/https://counterfunctions20200425175523.azurewebsites.net/api/devices";
   private readonly pushButtonUrl = "https://cors-anywhere.herokuapp.com/https://counterfunctions20200425175523.azurewebsites.net/api/pushButton";
-
+  private readonly getActiveEvents = "https://cors-anywhere.herokuapp.com/https://smokingdetectors.table.core.windows.net/CurrentAlerts?sv=2019-10-10&ss=t&srt=sco&sp=rwdlacu&se=2020-06-04T06:40:57Z&st=2020-05-21T22:40:57Z&spr=https&sig=V2cPiyk9d%2FKKJ0ddPQ8damTnNhHODrPPlFkCHNEiIps%3D"
+  private readonly getClientData = "https://cors-anywhere.herokuapp.com/https://smokingdetectors.table.core.windows.net/ClientsTable?"
+  //?$filter=RowKey%20eq%20"
+  private readonly connectionStringStorage = "sv=2019-10-10&ss=t&srt=sco&sp=rwdlacu&se=2020-06-04T06:40:57Z&st=2020-05-21T22:40:57Z&spr=https&sig=V2cPiyk9d%2FKKJ0ddPQ8damTnNhHODrPPlFkCHNEiIps%3D"
   private readonly counterId = 1;
 
   private hubConnection: signalR.HubConnection;
   public counter: number = 0;
-  public devices: string[] = [];
+  public devices: string[] = ["bla", "bla1"];
+  public events: JSON;
+  public page: string;
+  public displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  public dataSource: PeriodicElement[];
+  
+  public clientsData: object
+  public alerts: object
+  public ALERTS_DATA:  AlertNode[];
 
   constructor(private readonly http: HttpClient) {
+    this.dataSource = ELEMENT_DATA;
+    // this.dataSourceTree.data = TREE_DATA;
+    this.ALERTS_DATA = [];
+    
+
     const negotiateBody = { UserId: "SomeUser" };
 
-    this.http
-      .post<SignalRConnection>(this.negotiateUrl, JSON.stringify(negotiateBody), this.httpOptions)
-      .pipe(
-        map(connectionDetails =>
-          new signalR.HubConnectionBuilder().withUrl(`${connectionDetails.url}`, { accessTokenFactory: () => connectionDetails.accessToken }).build()
-        )
-      )
-      .subscribe(hub => {
-        this.hubConnection = hub;
-        hub.on("CounterUpdate", data => {
-          console.log(data);
-          this.counter = data.Count;
+    // this.http
+    //   .post<SignalRConnection>(this.negotiateUrl, JSON.stringify(negotiateBody), this.httpOptions)
+    //   .pipe(
+    //     map(connectionDetails =>
+    //       new signalR.HubConnectionBuilder().withUrl(`${connectionDetails.url}`, { accessTokenFactory: () => connectionDetails.accessToken }).build()
+    //     )
+    //   )
+    //   .subscribe(hub => {
+    //     this.hubConnection = hub;
+    //     hub.on("CounterUpdate", data => {
+    //       console.log(data);
+    //       this.counter = data.Count;
+    //     });
+    //     hub.start();
+    //   });
+
+    // this.http.get<Counter>(this.getCounterUrl + "/" + this.counterId).subscribe(cloudCounter => {
+    //   console.log(cloudCounter);
+    //   this.counter = cloudCounter.count;
+    // });
+
+    // this.http.get<string[]>(this.getIotDevicesUrl).subscribe(devices => {
+    //   console.log(devices);
+    //   this.devices = devices;
+    // });
+
+    this.http.get<JSON>(this.getActiveEvents, this.httpOptions).subscribe(Alerts => {  
+      for (var key in Alerts["value"]) {
+        console.log(Alerts)
+        this.http.get<JSON>(this.getClientData + this.connectionStringStorage, this.httpOptions).subscribe(clientsData => {
+          
+          this.CreateAlerts(Alerts["value"][key], clientsData["value"]["0"])
         });
-        hub.start();
-      });
-
-    this.http.get<Counter>(this.getCounterUrl + "/" + this.counterId).subscribe(cloudCounter => {
-      console.log(cloudCounter);
-      this.counter = cloudCounter.count;
+      }
+      console.log(this.ALERTS_DATA)
     });
+    
+  }
 
-    this.http.get<string[]>(this.getIotDevicesUrl).subscribe(devices => {
-      console.log(devices);
-      this.devices = devices;
-    });
+  public CreateAlerts(alertElement, clientElement): void {
+    console.log(alertElement)
+    console.log(clientElement)
+    this.ALERTS_DATA.push({alert_obj: alertElement, client_obj: clientElement})  
+    this.printAlertsData(this.ALERTS_DATA["0"]["alert_obj"].device_id)    
+  }
+
+  public printAlertsData(al): void {
+    console.log(al)
   }
 
   public increaseCounter(): void {
@@ -79,4 +222,25 @@ export class AppComponent {
         console.log("called push button");
       });;      
   }
+
+  public changeToPage(pageName: string): void {
+    this.page = pageName
+  }
+
+  changeDisabled(): boolean{
+    this.checked = !this.checked;
+    console.log(this.checked)
+    return this.checked;
+  }
+
+  addAlert(event: object, client: object): void {
+    console.log(this.injured)
+    console.log(event)
+    console.log(client)
+  }
+
+
+
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,42 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppComponent } from "./app.component";
+import {MatMenuModule} from '@angular/material/menu';
+import {MatTableModule} from '@angular/material/table';
+import {MatListModule} from '@angular/material/list';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSelectionList, MatListOption} from '@angular/material/list';
+import {CommonModule} from '@angular/common';
+import {MatTreeModule} from '@angular/material/tree';
+import {MatIconModule} from '@angular/material/icon';
+import {FlatTreeControl} from '@angular/cdk/tree';
+import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
+import {MatButtonModule} from '@angular/material/button';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+
+
+
+
+
+
+
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, MatMenuModule, BrowserAnimationsModule, MatTableModule,
+     MatListModule, CommonModule, MatTreeModule, MatIconModule, MatButtonModule, MatExpansionModule,
+     MatCheckboxModule, MatInputModule, MatFormFieldModule, FormsModule],
+  exports: [MatSelectionList, MatListOption],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
+
+
