66d35ea4ce0dbccc7cd055f64816a84aced563fc
Bar Naim <bar.naim@similarweb.com>
2020-05-27 20:21:18 +0300

    alertupdate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -93,10 +93,26 @@ interface ExampleFlatNode {
   level: number;
 }
 
+interface CurrentAlertsObject {
+  PartitionKey: string;
+  RowKey: string;
+  latitude: string;
+  longitude: string;
+}
+
+interface ClientsObject {
+  PartitionKey: string;
+  RowKey: string;
+  email: string;
+  name: string;
+  password: string;
+  phone_number: string;
+}
+
 
 interface AlertNode {
-  alert_obj: Object;
-  client_obj: Object
+  alert_obj: CurrentAlertsObject;
+  client_obj: ClientsObject;
 }
 
 
@@ -119,7 +135,6 @@ interface AlertNode {
 export class AppComponent {
   panelOpenState = false;
   checked = false;
-  injured = ""
 
   
 
@@ -131,6 +146,8 @@ export class AppComponent {
   private readonly pushButtonUrl = "https://cors-anywhere.herokuapp.com/https://counterfunctions20200425175523.azurewebsites.net/api/pushButton";
   private readonly getActiveEvents = "https://cors-anywhere.herokuapp.com/https://smokingdetectors.table.core.windows.net/CurrentAlerts?sv=2019-10-10&ss=t&srt=sco&sp=rwdlacu&se=2020-06-04T06:40:57Z&st=2020-05-21T22:40:57Z&spr=https&sig=V2cPiyk9d%2FKKJ0ddPQ8damTnNhHODrPPlFkCHNEiIps%3D"
   private readonly getClientData = "https://cors-anywhere.herokuapp.com/https://smokingdetectors.table.core.windows.net/ClientsTable?"
+  private readonly addEvent = "http://localhost:7071/api/add-event"
+  private readonly deleteCurrent = "http://localhost:7071/api/delete-alert"
   //?$filter=RowKey%20eq%20"
   private readonly connectionStringStorage = "sv=2019-10-10&ss=t&srt=sco&sp=rwdlacu&se=2020-06-04T06:40:57Z&st=2020-05-21T22:40:57Z&spr=https&sig=V2cPiyk9d%2FKKJ0ddPQ8damTnNhHODrPPlFkCHNEiIps%3D"
   private readonly counterId = 1;
@@ -141,14 +158,17 @@ export class AppComponent {
   public events: JSON;
   public page: string;
   public displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  public dataSource: PeriodicElement[];
+  public dataSource: object[];
+  public errorSubmit: string;
+  public details: string = null
+  public injured: number = null;
+  public url: string;
   
   public clientsData: object
   public alerts: object
   public ALERTS_DATA:  AlertNode[];
 
   constructor(private readonly http: HttpClient) {
-    this.dataSource = ELEMENT_DATA;
     // this.dataSourceTree.data = TREE_DATA;
     this.ALERTS_DATA = [];
     
@@ -191,6 +211,13 @@ export class AppComponent {
       }
       console.log(this.ALERTS_DATA)
     });
+
+    // this.http.post(this.addEvent + "/4/1/Tel_Aviv/34353532/3435435/16:00/true/bla/6", this.httpOptions).toPromise()
+    // .catch(e => console.log(e));
+
+    // this.http.get<JSON>(this.deleteCurrent + "/2", this.httpOptions) .subscribe(() => {
+    //   console.log("delete alert");
+    // });;    
     
   }
 
@@ -198,7 +225,7 @@ export class AppComponent {
     console.log(alertElement)
     console.log(clientElement)
     this.ALERTS_DATA.push({alert_obj: alertElement, client_obj: clientElement})  
-    this.printAlertsData(this.ALERTS_DATA["0"]["alert_obj"].device_id)    
+    this.printAlertsData(this.ALERTS_DATA["0"]["alert_obj"])    
   }
 
   public printAlertsData(al): void {
@@ -238,6 +265,26 @@ export class AppComponent {
     console.log(event)
     console.log(client)
   }
+
+  submitEvent(PartitionKey: string, RowKey: string, latitude: string, longitude: string, time: string,
+    is_false_alarm_str: boolean, event_details: string, num_of_injured: number): void{
+      console.log(PartitionKey)
+      this.url = "/" + PartitionKey + "/" + RowKey + "/" + latitude +
+      "/" + longitude + "/" + time + "/" + is_false_alarm_str + "/" + event_details + "/" 
+      + num_of_injured;
+
+      console.log(this.url)
+
+      this.http.post(this.addEvent + this.url, this.httpOptions).toPromise()
+      .catch(e =>
+        this.errorSubmit = stringify(e));
+
+      // this.http.get<JSON>(this.deleteCurrent + "/" + RowKey, this.httpOptions) .subscribe(() => {
+      //     console.log("delete alert");});
+
+      
+
+    }
 
 
 
